55baa08ce95523304571e38839fdd3d12f216b25
Mikhail Matiunin <mikhail.matiunin@doctorina.com>
2025-06-12 15:47:10 +0400

    Refactor launch configuration for Dart unit testing and add initial test files

---
+++ test/parser/parser_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() => group('Parse', () {
+      test('Example Test', () {
+        expect(1 + 1, equals(2));
+      });
+
+      test('Another Example Test', () {
+        expect('Hello, World!', contains('World'));
+      });
+    });
